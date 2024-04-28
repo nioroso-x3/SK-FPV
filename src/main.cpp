940c225ab6faa9dcdd48873f9366608637fe9a9b
nioroso-x3 <nioroso_x3@hotmail.com>
2024-04-28 03:13:44 -0400

    Fixed the hud to the head, very basic text data and unfinished. Maybe I will rip out the cairo hud from wfb-ng-osd or fpvue.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,43 @@
 using namespace sk;
 using namespace mavsdk;
 
+
+#define HUD_COLOR cv::Scalar(0,255,0,160)
+#define HUD_COLOR_CLR cv::Scalar(0,0,0,0)
+
+//drawing functions to be moved to another file
+
+//draws line, returns origin and end point of the line drawn as a 4 value scalar x1,y1,x2,y2
+cv::Scalar 
+draw_line(cv::Mat&   mat, //matrix 
+          cv::Point  ori,    //origin, either left top or center of the line
+          float      length,  //length
+          float      angle,  //angle in rads
+          int        thc,    // thickness
+          cv::Scalar color, // color
+          bool       center_ori){ //true for center or false for origin at left top
+  if(center_ori){
+    cv::Point right;
+    cv::Point left;
+    right.x = int(ori.x + length/2 * cos(angle) );
+    right.y = int(ori.y + length/2 * sin(angle) );
+    left.x=(ori.x - (right.x - ori.x));
+    left.y=(ori.y - (right.y - ori.y));
+    cv::line(mat,left,right,color,thc,cv::LINE_AA);
+    return cv::Scalar(left.x,left.y,right.x,right.y);
+  }
+  else{
+    cv::Point right;
+    cv::Point left;
+    right.x = int(ori.x + length * cos(angle) );
+    right.y = int(ori.y + length * sin(angle) );
+    cv::line(mat,ori,right,color,thc,cv::LINE_AA);
+    return cv::Scalar(ori.x,ori.y,right.x,right.y);
+  }
+}
+
+
+
 //pose_t controls location, X (left/right), Y(up/down), Z(front/back). The second argument is a quartenion, you can search for calculators if you want to change the screen angle and rotation.
 
 //Globals so all threads can access them
@@ -69,13 +106,42 @@ Telemetry::EulerAngle vh_att;
 
 //vehicle batteries 0 and 1
 //attributes are id, voltage_v, current_battery_a
-Telemetry::Battery vh_bat;
+Telemetry::Battery vh_bat0;
+Telemetry::Battery vh_bat1;
+
+//vehicle airspeed_m_s, throttle_percentage and climb_rate_m_s
+Telemetry::FixedwingMetrics vh_fwing;
+
+//vehicle gps info, num_satellites and fix_type
+Telemetry::GpsInfo vh_gpsi;
+
+//vehicle raw gps info, velocity_m_s is the ground speed
+Telemetry::RawGps vh_gpsr;
+
+
+//vehicle status messages
+//should hold the last status
+int status_counter = 0;
+Telemetry::StatusText vh_st_text;
+
 
 
 //should make this only one function but whatever
 void frame_grabber0()
 {
-  while(true){
+  buffer0[0] = cv::Mat(512,512,CV_8UC4,HUD_COLOR_CLR);
+  buffer0[1] = cv::Mat(512,512,CV_8UC4,HUD_COLOR_CLR);
+
+  std::cerr << "Waiting for video 1" << std::endl;
+
+  bool cap0_open = cap0.open("udpsrc port=5600 caps=application/x-rtp, media=video,clock-rate=90000, encoding-name=H264 ! queue ! rtph264depay ! avdec_h264 ! videoconvert ! video/x-raw,format=RGBA ! appsink max-buffers=2 drop=true sync=false",cv::CAP_GSTREAMER);
+  
+  if(!cap0.isOpened()){
+      std::cerr << "Error opening video 1" << std::endl;
+  }
+
+  std::cerr << "Starting video 1" << std::endl;
+  while(cap0.isOpened()){
     cap0.read(buffer0[!cur_buffer0]);
     cur_buffer0 = !cur_buffer0;
   }
@@ -83,41 +149,147 @@ void frame_grabber0()
 
 void frame_grabber1()
 {
-  while(true){
+  buffer1[0] = cv::Mat(512,512,CV_8UC4,HUD_COLOR_CLR);
+  buffer1[1] = cv::Mat(512,512,CV_8UC4,HUD_COLOR_CLR);
+
+  std::cerr << "Waiting for video 2" << std::endl;
+
+  bool cap1_open = cap1.open("udpsrc port=5601 caps=application/x-rtp, media=video,clock-rate=90000, encoding-name=H264 ! queue ! rtph264depay ! avdec_h264 ! videoconvert ! video/x-raw,format=RGBA ! appsink max-buffers=2 drop=true sync=false",cv::CAP_GSTREAMER);
+  
+  if(!cap1.isOpened()){
+      std::cerr << "Error opening video 2" << std::endl;
+  }
+
+  std::cerr << "Starting video 2" << std::endl;
+  while(cap1.isOpened()){
     cap1.read(buffer1[!cur_buffer1]);
     cur_buffer1 = !cur_buffer1;
   }
 }
 
+void mavlink_thread()
+{
+  std::cerr << "Started mavlink setup thread" << std::endl;
+  Mavsdk mavsdk{Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation}};
+  ConnectionResult connection_result = mavsdk.add_any_connection("udp://0.0.0.0:14551");
+
+  if (connection_result != ConnectionResult::Success) {
+      std::cerr << "Connection failed: " << connection_result << '\n';
+  }
+
+  auto system = mavsdk.first_autopilot(3.0);
+  while(!system) {
+      std::cerr << "Connection timeout, waiting 5 seconds " << std::endl;
+      system = mavsdk.first_autopilot(5.0);
+  }
+
+
+   // Instantiate plugins.
+  auto telemetry = mavsdk::Telemetry{system.value()};
+
+  telemetry.subscribe_position([](Telemetry::Position position) {
+     vh_pos = position;
+  });
+  telemetry.subscribe_attitude_euler([](Telemetry::EulerAngle att){
+     vh_att = att;
+  });
+  telemetry.subscribe_battery([](Telemetry::Battery bat){
+     if (bat.id == 0){
+        vh_bat0 = bat;
+     }
+     if (bat.id == 1){
+        vh_bat1 = bat;
+     }
+  });
+  telemetry.subscribe_fixedwing_metrics([](Telemetry::FixedwingMetrics fwing){
+     vh_fwing = fwing;
+  });
+  telemetry.subscribe_gps_info([](Telemetry::GpsInfo gpsi){
+     vh_gpsi = gpsi;
+  });
+   telemetry.subscribe_raw_gps([](Telemetry::RawGps gpsr){
+     vh_gpsr = gpsr;
+  });
+ 
+
+  telemetry.subscribe_status_text([](Telemetry::StatusText st_text){
+     if (st_text.text != vh_st_text.text){
+       vh_st_text = st_text;
+       status_counter = 20*15;
+     }
+  });
+
+
+  std::cerr << "Mavlink thread going to sleep" << std::endl;
+  while(true){
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+
+
+}
+
 void draw_hud()
 {
-  hud_buf[0] = cv::Mat(1024,1024,CV_8UC4,cv::Scalar(0,0,0,0));
-  hud_buf[1] = cv::Mat(1024,1024,CV_8UC4,cv::Scalar(0,0,0,0));
+
+  hud_buf[0] = cv::Mat(1024,1024,CV_8UC4,HUD_COLOR_CLR);
+  hud_buf[1] = cv::Mat(1024,1024,CV_8UC4,HUD_COLOR_CLR);
+
+  std::cerr << "Starting HUD thread" << std::endl;
+  float r,p = 0;
   while(true){
       cv::Mat& cur_frame = hud_buf[!cur_hud_buf];
       //clear buffer
-      cur_frame.setTo(cv::Scalar(0,0,0,0));
+      cur_frame.setTo(HUD_COLOR_CLR);
       //copy current values
       Telemetry::Position c_pos = vh_pos;
       Telemetry::EulerAngle c_att = vh_att;
-      Telemetry::Battery c_bat = vh_bat;
+      Telemetry::Battery c_bat0 = vh_bat0;
+      Telemetry::Battery c_bat1 = vh_bat1;
+      Telemetry::FixedwingMetrics c_fw = vh_fwing;
+      Telemetry::RawGps c_gpsr = vh_gpsr;
+      Telemetry::StatusText st_text = vh_st_text;
 
       
       //draw roll
-      float r = c_att.roll_deg * M_PI /180.0;
-
-      cv::Point center(512,512);
-      cv::Point right;
-      cv::Point left;
-      right.x = int(center.x + 256 * cos(r) );
-      right.y = int(center.y + 256 * sin(r) );
-      left.x=(512 - (right.x - 512));
-      left.y=(512 - (right.y - 512));
-      cv::line(cur_frame,left,right,cv::Scalar(0,255,0,160),2,cv::LINE_AA);
-      
+      r = -c_att.roll_deg * M_PI /180.0;
+      draw_line(cur_frame,cv::Point(512,512),512,r,2,HUD_COLOR,true);
+      draw_line(cur_frame,cv::Point(512,512),96,r,4,HUD_COLOR_CLR,true);
       //draw pitch
-      float p = c_att.pitch_deg * M_PI / 180.0;
+      p = c_att.pitch_deg * M_PI / 180.0;
 
+
+
+      //using text as placeholders
+      cv::putText(cur_frame,"PITCH:",cv::Point(100,512),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(int(c_att.pitch_deg)),cv::Point(210,512),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+
+      cv::putText(cur_frame,"ALT:",cv::Point(770,512),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_pos.relative_altitude_m),cv::Point(880,512),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+ 
+      cv::putText(cur_frame,"AS:",cv::Point(770,542),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_fw.airspeed_m_s),cv::Point(880,542),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+ 
+      cv::putText(cur_frame,"GS:",cv::Point(770,572),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_gpsr.velocity_m_s),cv::Point(880,572),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+    
+      
+      cv::putText(cur_frame,"VOLT0:",cv::Point(10,800),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_bat0.voltage_v),cv::Point(120,800),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+
+      cv::putText(cur_frame," AMP0:",cv::Point(10,830),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_bat0.current_battery_a),cv::Point(120,830),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+
+      cv::putText(cur_frame,"VOLT1:",cv::Point(10,860),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_bat1.voltage_v),cv::Point(120,860),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+
+      cv::putText(cur_frame," AMP1:",cv::Point(10,890),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+      cv::putText(cur_frame,std::to_string(c_bat1.current_battery_a),cv::Point(120,890),cv::FONT_HERSHEY_DUPLEX,1.0,HUD_COLOR,2);
+
+      //draw message if needed      
+      if (status_counter){
+        status_counter -= 1;
+
+      }
 
 
       //flip buffers
@@ -139,38 +311,7 @@ model_t main_screen;
 
 
 
-int main(void) {
-//mavlink globals
-
-  Mavsdk mavsdk{Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation}};
-  ConnectionResult connection_result = mavsdk.add_any_connection("udp://0.0.0.0:14540");
-
-   if (connection_result != ConnectionResult::Success) {
-       std::cerr << "Connection failed: " << connection_result << '\n';
-       return 1;
-   }
-
-   auto system = mavsdk.first_autopilot(3.0);
-   while(!system) {
-       std::cerr << "Connection timeout on udp://0.0.0.0:14551, retrying " << std::endl;
-       system = mavsdk.first_autopilot(3.0);
-       std::this_thread::sleep_for(std::chrono::seconds(1));
-   }
-
-
-   // Instantiate plugins.
-   auto telemetry = mavsdk::Telemetry{system.value()};
-
-   telemetry.subscribe_position([](Telemetry::Position position) {
-      vh_pos = position;
-   });
-   telemetry.subscribe_attitude_euler([](Telemetry::EulerAngle att){
-      vh_att = att;
-   });
-   telemetry.subscribe_battery([](Telemetry::Battery bat){
-      vh_bat = bat;
-   });
-
+int main(int argc, char* argv[]) {
 
   //initialize stereokit
   sk_settings_t settings = {};
@@ -214,32 +355,16 @@ int main(void) {
   material_set_texture(hud_mat,"diffuse",hud_tex);
   material_set_transparency(hud_mat,transparency_blend);
 
-  //Start your streams first, or the app will block waiting for them to open.
-  //You can use vaapih264dec and vaapipostprocess to convert to RGBA in the GPU on AMD cards. Sterokit only supports RGBA32 textures
-  std::cout << "Starting video 1" << std::endl;
-  bool cap0_open = cap0.open("udpsrc port=5600 caps=application/x-rtp, media=video,clock-rate=90000, encoding-name=H264 ! queue ! rtph264depay ! avdec_h264 ! videoconvert ! video/x-raw,format=RGBA ! appsink max-buffers=2 drop=true sync=false",cv::CAP_GSTREAMER);
-  
-  if(!cap0.isOpened()){
-      std::cout << "Error opening video 1" << std::endl;
-      return -1;
-  }
 
-  std::cout << "Starting video 2" << std::endl;
-
-  bool cap1_open = cap1.open("udpsrc port=5601 caps=application/x-rtp, media=video,clock-rate=90000, encoding-name=H264 ! queue ! rtph264depay ! avdec_h264 ! videoconvert ! video/x-raw,format=RGBA ! appsink max-buffers=2 drop=true sync=false",cv::CAP_GSTREAMER);
-  if(!cap1.isOpened()){
-      std::cout << "Error opening video 2" << std::endl;
-      return -1;
-  }
-  
-  std::cout << "Starting frame threads" << std::endl;
-
+  std::cout << "Starting threads" << std::endl;
   std::thread t0(&frame_grabber0);
   std::thread t1(&frame_grabber1);
   std::thread t2(&draw_hud);
+  std::thread t3(&mavlink_thread);
   t0.detach();
   t1.detach();
   t2.detach();
+  t3.detach();
 
 
   std::cout << "Starting main loop" << std::endl;
@@ -254,6 +379,7 @@ int main(void) {
         if(!frame1.empty()){
           tex_set_colors(vid1,frame1.cols,frame1.rows,(void*)frame1.datastart);
         }
+  
         if(!hud_fr.empty()){
           tex_set_colors(hud_tex,hud_fr.cols,hud_fr.rows,(void*)hud_fr.datastart);
         }
@@ -268,16 +394,18 @@ int main(void) {
 
 
         //setup hud
-        hud_pose = *input_head();
-        hud_pose.position.z -= 0.2f;
+        vec3 at = input_head()->position + input_head()->orientation * vec3_forward * 0.7f;
+        quat ori = quat_lookat(input_head()->position,at);
+
         ui_handle_begin("HUD", hud_pose, mesh_get_bounds(hud_mesh), false);
-        render_add_mesh(hud_mesh, hud_mat, matrix_trs(hud_pose.position,{0,0,0,1},vec3_one));
+        render_add_mesh(hud_mesh, hud_mat, matrix_trs(at,ori,vec3_one));
         ui_handle_end();
          
         if(cnt % 100 == 0){
             //std::cout << hud_pose.orientation.x << " " << hud_pose.orientation.y << " " << hud_pose.orientation.z << " " << hud_pose.orientation.w << std::endl << std::endl;
             //std::cout << hud_pose.position.x << " " << hud_pose.position.y << " " << hud_pose.position.z << std::endl << std::endl;
-            std::cout << vh_att.roll_deg << " " << vh_att.pitch_deg << std::endl;
+            //std::cout << vh_att.roll_deg << " " << vh_att.pitch_deg << std::endl;
+            //std::cout << ori.x << " " << ori.y << " " << ori.z << " "<< ori.w << std::endl;
         }
         
         cnt++;
@@ -286,6 +414,7 @@ int main(void) {
   t0.join();
   t1.join();
   t2.join();
+  t3.join();
   cap0.release();
   cap1.release();
   //cap2.release();
